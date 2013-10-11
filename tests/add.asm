5ac48d3530864d10ad10e523d7f92d8498636011
Thomas Cort <linuxgeek@gamil.com>
2013-10-11 13:47:18 -0400

    as: support alternate commenting syntax.
    Allow ';' and '#' to be used where "//" is used. These
    commenting styles are used in some examples on the Internet.

--- tests/add.asm
+++ tests/add.asm
@@ -1,7 +1,7 @@
-INP
-STA DATA
-INP
-ADD DATA
-OUT
-HLT
-DATA DAT
+INP		# input 1st operand
+STA DATA	# store 1st operand
+INP		# input 2nd operand
+ADD DATA	# sum operands
+OUT		# output result
+HLT		# end of program
+DATA DAT	# storage
--- tests/load-store.asm
+++ tests/load-store.asm
@@ -1,11 +1,11 @@
-INP
-STA FIRST
-INP
-STA SECOND
-LDA FIRST
-OUT
-LDA SECOND
-OUT
-HLT
-FIRST DAT
-SECOND DAT
+INP		; get the first input
+STA FIRST	; store it in FIRST
+INP		; get the second input
+STA SECOND	; store it in SECOND
+LDA FIRST	; load FIRST
+OUT		; output value
+LDA SECOND	; load SECOND
+OUT		; output value
+HLT		; end of program
+FIRST DAT	; storage
+SECOND DAT	; storage
